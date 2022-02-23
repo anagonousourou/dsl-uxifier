03d00635a5ad5f08516f2e8a58cfd3d64b603b84
Jonas Vihoalé ANIGLO <46029829+JIV-DLS@users.noreply.github.com>
2022-02-23 12:10:03 +0100

    Create App.vue

---
+++ uxfier/app/CartApplication/src/App.vue
@@ -0,0 +1,25 @@
+<template><page1/></template><script>
+import page1 from './components/page-name-1.vue';
+            import '@vaadin/text-field';
+            import '@vaadin/checkbox';
+            import '@vaadin/combo-box';
+            import '@vaadin/email-field';
+            import '@vaadin/date-picker';
+            import '@vaadin/date-time-picker';
+            import '@vaadin/button';
+            import '@vaadin/icon';
+            import '@vaadin/message-input';
+            import '@vaadin/password-field';
+            import '@vaadin/time-picker';
+            import '@vaadin/upload';
+            import '@vaadin/radio-group';
+            import '@vaadin/accordion';
+            import '@vaadin/vertical-layout';
+            import '@vaadin/horizontal-layout';
+import '@vaadin/integer-field';
+
+import '@vaadin/text-area';
+        export default {
+            name: 'App',components :{page1,}}
+</script><style>
+</style>
